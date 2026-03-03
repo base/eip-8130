ccaae0d112dae5bab1ab16e5d970ca82b8e0a1a8
Conner Swenberg <conner.swenberg@coinbase.com>
2026-03-02 17:46:20 -0800

    Add receivers and 1271 to DefaultAccount

--- src/accounts/DefaultAccount.sol
+++ src/accounts/DefaultAccount.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import {UUPSUpgradeable} from "openzeppelin/proxy/utils/UUPSUpgradeable.sol";
+import {Receiver} from "solady/accounts/Receiver.sol";
 
 import {AccountConfiguration} from "../AccountConfiguration.sol";
 import {IInitialized} from "./IInitialized.sol";
 
-contract DefaultAccount is IInitialized, UUPSUpgradeable {
+contract DefaultAccount is IInitialized, UUPSUpgradeable, Receiver {
     struct Call {
         address target;
         bytes data;
@@ -17,11 +18,6 @@ contract DefaultAccount is IInitialized, UUPSUpgradeable {
 
     constructor(address accountConfiguration) {
         ACCOUNT_CONFIGURATION = AccountConfiguration(accountConfiguration);
-    }
-
-    function initialized() external pure virtual returns (bool) {
-        // TODO: weird to have this implicitly true?
-        return true;
     }
 
     modifier onlyAdmin() {
@@ -36,6 +32,16 @@ contract DefaultAccount is IInitialized, UUPSUpgradeable {
             (bool success, bytes memory data) = calls[i].target.call{value: calls[i].value}(calls[i].data);
             require(success);
         }
+    }
+
+    function isValidSignature(bytes32 hash, bytes memory signature) external view returns (bool) {
+        (bytes32 ownerId, bytes memory data) = abi.decode(signature, (bytes32, bytes));
+        return ACCOUNT_CONFIGURATION.verifyIntent(address(this), ownerId, hash, data);
+    }
+
+    /// @dev Only initialization required are owner additions handled by AccountConfiguration
+    function initialized() external pure virtual returns (bool) {
+        return true;
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyAdmin {}
