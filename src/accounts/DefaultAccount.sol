2ad0b3c6d5d46e385a616c720399d44d4206e4c4
Chris Hunter <chris.hunter@coinbase.com>
2026-03-20 10:33:47 -0400

    Upgradable

--- src/accounts/DefaultAccount.sol
+++ src/accounts/DefaultAccount.sol
@@ -76,7 +76,7 @@ contract DefaultAccount is Receiver {
     /// @param hash The digest to verify
     /// @param signature Auth data in verifier || data format
     /// @return magicValue 0x1626ba7e if valid, 0xffffffff otherwise
-    function isValidSignature(bytes32 hash, bytes calldata signature) external view returns (bytes4) {
+    function isValidSignature(bytes32 hash, bytes calldata signature) external view virtual returns (bytes4) {
         (bool valid,,) = ACCOUNT_CONFIGURATION.verifySignature(address(this), hash, signature);
         if (!valid) return bytes4(0xFFFFFFFF);
         return bytes4(0x1626ba7e);
