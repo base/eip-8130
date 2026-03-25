a0ce1a4023680367a2ad490ed215252d9a52b1dc
Conner Swenberg <conner.swenberg@coinbase.com>
2026-03-25 16:00:56 -0700

    Remove native verifiers

--- src/AccountConfiguration.sol
+++ src/AccountConfiguration.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import {INativeVerifiers} from "./interfaces/INativeVerifiers.sol";
 import {IVerifier} from "./interfaces/IVerifier.sol";
 
 /// @notice Account Configuration system contract for EIP-8130.
